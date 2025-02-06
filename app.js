674421345c0830790485cd7492d91ea7b1dd0822
mgkaunggithub@gmail.com <mgkaunggithub@gmail.com>
2025-02-07 00:00:43 +0630

    mongoose setup

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+require('./config/database');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const chalk = require('chalk');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+
 
 const app = express();
+// middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet()); 
+app.use(cors());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Node JS Express With Mongoose DB');
---
+++ config/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGOOSE_URL);
+
+mongoose.connection.on('connected', () => {
+    console.log(chalk.cyan('Connected to MongoDB'));
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(chalk.red('Error connecting to MongoDB:', err));
+});
